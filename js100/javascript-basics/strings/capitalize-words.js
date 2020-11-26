9e70365ca183a0fb6149b09d61c3ee6decbe7a96
Khalid Sahlqeath <khalid.sahlqeath@outlook.com>
2020-11-26 14:43:17 +0100

    strings exercises done

--- js100/javascript-basics/strings/capitalize-words.js
+++ js100/javascript-basics/strings/capitalize-words.js
@@ -1,17 +1,12 @@
-function capitalize(string) {
-  let words = string.split(' ');
-  let capitalizedWords = []
-  let characters = [];
-  words.forEach(element => {       
-    characters = element.split('');    
-    console.log(characters[0].toUpperCase());
-    //console.log(words);
-    //capitalizedWords.push(element.toUpperCase())       
-  });    
-  return capitalizedWords;
+function capitalize(string) {  
+  let words = string.split(' ');  
+  let capitalizedWords = [];
+  words.forEach(word => {    
+    capitalizedWords.push(word[0].toUpperCase() + word.slice(1));    
+  })
+  return capitalizedWords.join(' ');
 }
 
 let sentence = 'launch school tech & talk';
-//let oneWord = 'hello';
-//oneWord.split('').forEach(c => console.log(c));
+
 console.log(capitalize(sentence));
